aa1412bfbf4f6acd977e9b40e36284a700579697
nicolasluarte <l.luarte@uc.cl>
2021-03-22 00:50:12 -0300

    added apt packages for opencv to work

--- mainUI/data/linux_scripts/setPackages.sh
+++ mainUI/data/linux_scripts/setPackages.sh
@@ -5,6 +5,8 @@ IPS="$HOME/nbolab_UI/mainUI/data/linux_scripts/raspberrypi.txt"
 # NFS
 parallel --tag --nonall --slf $IPS sudo apt-get update
 parallel --tag --nonall --slf $IPS sudo apt-get install nfs-common
+# for opencv to work
+parallel --tag --nonall --slf $IPS sudo apt-get install -y libatlas-base-dev libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev  libqtgui4  libqt4-test
 
 # Processing
 parallel --tag --nonall --slf $IPS sudo apt-get install git
